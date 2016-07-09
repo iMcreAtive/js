b9076711ec1ecb9f68ebee686a7bcc63a0406575
iMcreAtive <igormostovenko@gmail.com>
2016-07-09 19:20:57 +0300

    add stylus support, generic css moved to css/main.styl

--- entry.js
+++ entry.js
@@ -1,7 +1,9 @@
+// parts
 require("./parts/navigation/navigation.js");
 require("./parts/invision-form/invision-form.js");
 require("./parts/apple-footer/apple-footer.js");
 require("./parts/hero/hero.js");
 require("./parts/oculus/oculus.js");
-
+// other
 require("./js/responsive.js");
+require("./css/main.styl");
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
     entry: "./entry.js",
     output: {
@@ -6,8 +8,15 @@ module.exports = {
     },
     module: {
         loaders: [
-          { test: /\.css$/, loader: "style-loader!css-loader"},
-          { test: /\.(png|jpg|gif)$/, loader: "url-loader?name=/bundle/[name].[ext]" },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' },
+            { test: /\.(png|jpg|gif)$/, loader: "url-loader?name=/bundle/[name].[ext]" },
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin("[name].css")
+    ]
 };

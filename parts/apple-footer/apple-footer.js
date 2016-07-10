bab02ffadaadfeed109a3a8bb780a305895ea35d
iMcreAtive <igormostovenko@gmail.com>
2016-07-10 18:50:01 +0300

    apple footer and invision form

--- parts/apple-footer/apple-footer.js
+++ parts/apple-footer/apple-footer.js
@@ -1,6 +1,7 @@
 var $ = require("jquery");
 require("./apple-footer.css");
 
+// af
 $('.footer-toggle').on('click', function(){
 	if(window.innerWidth <= 720){
 		$($(this).data('id')).toggle(200);
